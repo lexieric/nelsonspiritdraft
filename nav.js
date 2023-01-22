b18f01a952874a5e7a00710b33ef3af8a162dba7
Konstantin <khaehnelt@yahoo.co.uk>
2023-01-22 01:24:16 +0000

    Shifting CSS to try make it work

--- nav.js
+++ nav.js
@@ -1,10 +1,9 @@
-//
-//    Toggle Mobile Navigation
-//
+
+// Toggle navigation open 
+
 const navbarMenu = document.querySelector("#navigation #navbar-menu");
 const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
 
 hamburgerMenu.addEventListener('click', function() {
     navbarMenu.classList.toggle("open");
-    hamburgerMenu.classList.toggle("clicked");
 });
